5b58201781a88a17469ca843e106681f766a94a2
Charly <69594869+Charly31Z@users.noreply.github.com>
2022-11-11 23:09:13 -0600

    Testeo
    Solo agregue otro host como prueba
    
    Signed-off-by: Charly <69594869+Charly31Z@users.noreply.github.com>

--- websites/C/Cuevana 3/presence.ts
+++ websites/C/Cuevana 3/presence.ts
@@ -100,6 +100,75 @@ presence.on("UpdateData", async () => {
 			presenceData.smallImageText = strings.browsing;
 		}
 	}
+	
+	// Testing...
+	if (document.location.hostname === "m4.cuevana3.me") {
+		const presenceData: PresenceData = {
+			largeImageKey: "logo",
+		};
+
+		if (
+			document.location.pathname.match("^/[0-9]") ||
+			document.location.pathname.includes("/episodio")
+		) {
+			const titulo = document.querySelector("h1.Title").textContent,
+				subtitulo = document.querySelector("h2.SubTitle").textContent,
+				cover = document
+					.querySelector("div.backdrop > article > div.Image > figure > img")
+					.getAttribute("data-src");
+
+			if (!document.location.pathname.includes("/episodio")) {
+				presenceData.details = titulo;
+				if (subtitulo === titulo) delete presenceData.state;
+				else presenceData.state = subtitulo;
+				presenceData.buttons = [
+					{ label: "Ver Película", url: window.location.href },
+				];
+				presenceData.largeImageKey = cover;
+			} else {
+				presenceData.details = titulo
+					.replace(document.querySelector("h1.Title > span").textContent, "")
+					.replace(/\s+/g, " ")
+					.trim();
+				presenceData.state = `${
+					document.querySelector("h1.Title > span").textContent
+				} ${subtitulo}`;
+				presenceData.buttons = [
+					{ label: "Ver Episodio", url: window.location.href },
+				];
+				presenceData.largeImageKey = cover;
+			}
+
+			if (iFrameVideo) {
+				strings = await getStrings();
+				(presenceData.smallImageKey = videoPaused ? "pause" : "play"),
+					(presenceData.smallImageText = videoPaused
+						? strings.pause
+						: strings.play),
+					(presenceData.startTimestamp = startTimestamp),
+					(presenceData.endTimestamp = endTimestamp);
+
+				if (videoPaused) {
+					delete presenceData.startTimestamp;
+					delete presenceData.endTimestamp;
+				}
+			}
+
+			presence.setActivity(presenceData, !videoPaused);
+		} else if (document.location.pathname.includes("/serie/")) {
+			presenceData.details = document.querySelector("h1.Title").textContent;
+			presenceData.smallImageKey = "browsing";
+			presenceData.smallImageText = strings.browsing;
+			presenceData.largeImageKey = document
+				.querySelector("div.backdrop > article > div.Image > figure > img")
+				.getAttribute("data-src");
+		} else {
+			presenceData.details = strings.browsing;
+			presenceData.smallImageKey = "browsing";
+			presenceData.smallImageText = strings.browsing;
+		}
+	}
+	// Here finish the testing
 
 	const presenceData: PresenceData = {
 		largeImageKey: "logo",
